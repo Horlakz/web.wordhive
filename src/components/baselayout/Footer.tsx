fe97ca00596c26db9f8a44c258ff278a3824201b
horlakz <officialhorlakz@gmail.com>
2023-07-14 16:15:10 +0100

    feat(src/components/baselayout/): adds check to see if window is not undefined

--- src/components/baselayout/Footer.tsx
+++ src/components/baselayout/Footer.tsx
@@ -6,6 +6,14 @@ import navLinks from "@/store/data/nav-links.json";
 import { socialLinks } from "@/store/data/social-links";
 
 const Footer = () => {
+  const [url, setUrl] = React.useState("");
+
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      setUrl(window.location.host);
+    }
+  }, []);
+
   return (
     <footer className="px-40 space-y-20">
       <section className="flex justify-between items-center">
@@ -44,7 +52,7 @@ const Footer = () => {
         </div>
 
         <span>
-          Copyright &copy;{new Date().getFullYear()} {window?.location?.host}
+          Copyright &copy;{new Date().getFullYear()} {url}
         </span>
 
         <span className="flex gap-1">
