7472829d127ce64880f3c475420a58687e2e397e
horlakz <officialhorlakz@gmail.com>
2023-07-14 12:03:04 +0100

    feat(src/components/baselayout/): designs header component

--- src/components/baselayout/Header.tsx
+++ src/components/baselayout/Header.tsx
@@ -1,16 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
+
+import navLinks from "@/store/data/nav-links.json";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
-    <header className="flex justify-between items-center capitalize px-20 py-6">
-      <h1 className="text-lg font-medium">Wordhive</h1>
-      <nav className="flex-center gap-2">
-        <span>home</span>
-        <span>about</span>
-        <span>faq</span>
+    <header className="flex justify-between items-center capitalize px-16 py-4 shadow-lg">
+      <Image src="/images/logo.png" width={40} height={40} alt="logo" />
+
+      <nav>
+        <ul className="flex-center gap-12">
+          {navLinks.map((link, i) => {
+            const isActive = pathname.startsWith(link.path);
+
+            return (
+              <li key={i}>
+                <Link
+                  className={isActive ? "text-primary" : "text-black"}
+                  href={link.path}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </nav>
 
-      <button className="px-2 py-1 bg-slate-300 rounded-lg">contact now</button>
+      <Link className="text-secondary" href={"/login"}>
+        Sign In
+      </Link>
     </header>
   );
 };
