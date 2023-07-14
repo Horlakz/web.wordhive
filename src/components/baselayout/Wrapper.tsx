957138321a964e5bfefa9b126fb5a5eb04069c06
horlakz <officialhorlakz@gmail.com>
2023-07-14 13:27:35 +0100

    feat(src/components/baselayout/): adds Footer to app layout Wrapper

--- src/components/baselayout/Wrapper.tsx
+++ src/components/baselayout/Wrapper.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { Toaster } from "react-hot-toast";
+
+import Footer from "./Footer";
 import Header from "./Header";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +16,7 @@ function Wrapper({ children }: { children: React.ReactNode }) {
       <Toaster />
       <Header />
       {children}
+      <Footer />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
