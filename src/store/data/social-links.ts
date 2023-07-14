269d0edbc4a5f1edd731387480495fdbe0878d78
horlakz <officialhorlakz@gmail.com>
2023-07-14 13:26:35 +0100

    feat(src/store/data/): creates data file to store footer social links

---
+++ src/store/data/social-links.ts
@@ -0,0 +1,17 @@
+import facebookCircleFill from "@/assets/icons/facebook-circle-fill.svg";
+import twitterFill from "@/assets/icons/twitter-fill.svg";
+import instagramFill from "@/assets/icons/instagram-fill.svg";
+
+export const socialLinks = [
+  {
+    icon: facebookCircleFill,
+    href: "https://www.facebook.com/",
+    name: "facebook",
+  },
+  { icon: twitterFill, href: "https://twitter.com/", name: "twitter" },
+  {
+    icon: instagramFill,
+    href: "https://www.instagram.com/",
+    name: "instagram",
+  },
+];
