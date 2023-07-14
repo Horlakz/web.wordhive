0628e6ae3f86f97e3e22ba80c2f02deba4265858
horlakz <officialhorlakz@gmail.com>
2023-07-14 20:07:58 +0100

    feat(src/app/): adds first section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+import Image from "next/image";
+
+import heroImage from "@/assets/images/hero.svg";
+import Button from "@/components/Button";
+
 export default function Home() {
   return (
-    <main className="p-40">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
-      obcaecati suscipit exercitationem at, veniam velit, id itaque vitae
+    <main className="py-16 px-14">
+      <section className="flex-center gap-20">
+        <div className="space-y-6">
+          <h4 className="text-7xl font-medium capitalize leading-[5.5rem]">
+            <span className="text-primary">Your Bridge to perfect&nbsp;</span>
+            <br />
+            <span className="text-secondary">expression</span>
+          </h4>
+
+          <p className="text-dark-600">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
+            ab quas fugit! Est error laudantium labore repellendus architecto
+            quo soluta dolorum similique veniam, placeat animi pariatur
+            distinctio facilis fugiat modi!
+          </p>
+          <Button>Get Started</Button>
+        </div>
+
+        <Image src={heroImage} width={500} height={500} alt="Hero" />
+      </section>
     </main>
   );
 }
