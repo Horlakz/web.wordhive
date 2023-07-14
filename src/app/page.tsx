fe4186c608ec8dea14029316461a4cd60fa4d72e
horlakz <officialhorlakz@gmail.com>
2023-07-14 13:27:50 +0100

    feat(src/app/): updates home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="p-40">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
       obcaecati suscipit exercitationem at, veniam velit, id itaque vitae
-      explicabo quaerat sequi veritatis quia dolorem sapiente quae labore eaque
-      adipisci laboriosam?<Link href="/about">Go to about page</Link>
     </main>
   );
 }
