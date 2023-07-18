205ab1189fb9223c9585455228c829d05232008f
horlakz <officialhorlakz@gmail.com>
2023-07-18 09:39:22 +0100

    feat(src/components/about/): adds responsiveness to component

--- src/components/about/InquirySection.tsx
+++ src/components/about/InquirySection.tsx
@@ -6,12 +6,12 @@ import Button from "@/components/Button";
 
 function InquirySection() {
   return (
-    <section className="px-48 sm:flex center gap-10 hidden">
+    <section className="sm:px-48 flex center gap-10">
       <div>
         <h5 className="text-2xl font-semibold text-primary">
           Have any Inquiries?
         </h5>
-        <p className="text-dark-600 mb-4 w-[35rem]">
+        <p className="text-dark-600 mb-4 sm:w-[35rem]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nihil
           ipsa quos similique alias. Deleniti optio, dolorum nostrum ab
           perspiciatis est fugiat, sed quaerat omnis eligendi quibusdam,
@@ -20,7 +20,13 @@ function InquirySection() {
         <Button>Contact us now!</Button>
       </div>
 
-      <Image src={customerCare} width={400} height={400} alt="customer care" />
+      <Image
+        src={customerCare}
+        width={400}
+        height={400}
+        alt="customer care"
+        className="sm:block hidden"
+      />
     </section>
   );
 }
