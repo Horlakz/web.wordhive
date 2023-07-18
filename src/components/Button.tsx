05325011d810f46d18df013a14e257300922eba1
horlakz <officialhorlakz@gmail.com>
2023-07-18 12:30:10 +0100

    feat(src/components/): adds props to add extra class and also add icon

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
   children: React.ReactNode;
+  icon?: React.ReactNode;
+  iconPosition?: "left" | "right";
+  className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  icon,
+  iconPosition = "left",
+  className,
+}) => {
   return (
-    <button className="rounded-md px-6 py-2.5 bg-primary text-white ">
+    <button
+      className={twMerge(
+        "rounded-md px-6 py-2.5 bg-primary text-white flex items-center",
+        className
+      )}
+    >
+      {icon && iconPosition === "left" && <span className="mr-2">{icon}</span>}
       {children}
+      {icon && iconPosition === "right" && <span className="ml-2">{icon}</span>}
     </button>
   );
 };
