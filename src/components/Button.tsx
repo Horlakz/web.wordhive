45f620ab78a9f2393189482b9bcbfd74948ddf06
horlakz <officialhorlakz@gmail.com>
2023-07-19 23:37:46 +0100

    feat(src/components/): adds variant, colorScheme and onClick props and functionalities to component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
+type Variant = "solid" | "outline";
+type ColorScheme =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "danger"
+  | "warning"
+  | "info";
+
 interface ButtonProps {
   children: React.ReactNode;
   icon?: React.ReactNode;
   iconPosition?: "left" | "right";
   className?: string;
+  variant?: Variant;
+  colorScheme?: ColorScheme;
+  onClick?: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -13,13 +25,27 @@ const Button: React.FC<ButtonProps> = ({
   icon,
   iconPosition = "left",
   className,
+  variant = "solid",
+  colorScheme = "primary",
+  onClick,
 }) => {
+  const getVariantClasses = () => {
+    switch (variant) {
+      case "outline":
+        return `border-2 border-${colorScheme} text-${colorScheme} bg-transparent`;
+      default:
+        return `bg-${colorScheme} text-white`;
+    }
+  };
+
   return (
     <button
       className={twMerge(
-        "rounded-md px-6 py-2.5 bg-primary text-white flex items-center",
+        "rounded-md px-6 py-2.5 flex items-center",
+        getVariantClasses(),
         className
       )}
+      onClick={onClick}
     >
       {icon && iconPosition === "left" && <span className="mr-2">{icon}</span>}
       {children}
