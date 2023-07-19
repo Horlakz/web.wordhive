fc90ebf7f11bfb86a6481546468b7fb23c25501b
horlakz <officialhorlakz@gmail.com>
2023-07-19 20:07:14 +0100

    feat(src/app/contact/): adds faq section and adds mobile responsiveness

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,12 +4,13 @@ import React from "react";
 import Button from "@/components/Button";
 import InputGroup from "@/components/InputGroup";
 import SendIcon from "@/assets/icons/send.svg";
+import Accordion from "@/components/Accordion";
 
 const ContactPage = () => {
   return (
-    <main className="p-20">
-      <section className="flex-center w-full gap-8">
-        <div className="w-3/5 py-10 px-6 space-y-3 bg-primary text-white">
+    <main className="sm:p-20 p-6 space-y-10">
+      <section className="flex-center sm:flex-row flex-col w-full gap-8">
+        <div className="sm:w-3/5 py-10 px-6 space-y-3 bg-primary text-white">
           <h3 className="text-3xl font-semibold">Contact Us</h3>
           <p className="">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
@@ -29,23 +30,38 @@ const ContactPage = () => {
           </div>
         </div>
 
-        <div className="w-full p-6 gap-x-6 gap-y-4 grid grid-cols-2">
+        <div className="w-full p-6 gap-x-6 gap-y-4 grid sm:grid-cols-2">
           <InputGroup.Input label="Full Name" />
           <InputGroup.Input label="Email" type="email" />
           <InputGroup.Input label="Phone" type="tel" />
           <InputGroup.Input label="Country" />
           <InputGroup.TextArea
-            className="col-span-2"
+            className="sm:col-span-2"
             label="Your Message"
             rows={5}
           />
           <Button
-            className="col-span-2 justify-center"
+            className="sm:col-span-2 justify-center"
             icon={<Image src={SendIcon} alt="icon" width={20} height={20} />}
             iconPosition="right"
           >
             Send Message
           </Button>
+        </div>
+      </section>
+
+      <section className="space-y-8">
+        <h3 className="text-2xl text-primary font-medium text-center">
+          Frequently Asked Questions
+        </h3>
+
+        <div className="grid sm:grid-cols-2 gap-6">
+          {[...new Array(7)].map(() => (
+            <Accordion
+              title="Hello World"
+              descr="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quae necessitatibus magni nobis unde ipsa totam quia reiciendis porro dolore mollitia distinctio qui in assumenda vero, architecto tempore natus molestiae!"
+            />
+          ))}
         </div>
       </section>
     </main>
