b017fc3425988d1c7b3a5199d1b9d9a37cf3eca3
horlakz <officialhorlakz@gmail.com>
2023-07-20 16:56:42 +0100

    feat(src/store/context/): add more context values

--- src/store/context/auth.tsx
+++ src/store/context/auth.tsx
@@ -1,12 +1,21 @@
 import { FC, createContext, useState } from "react";
 
+export enum VerificationType {
+  ConfirmEmail = "confirmEmail",
+  ForgotPassword = "forgotPassword",
+}
+
 interface AuthContextType {
   email: string;
   password: string;
   otp: string;
+  verificationType: VerificationType;
+  message: string;
   setEmail: (email: string) => void;
   setPassword: (password: string) => void;
   setOtp: (otp: string) => void;
+  setVerificationType: (verificationType: VerificationType) => void;
+  setMessage: (message: string) => void;
 }
 
 interface AuthProviderProps {
@@ -17,23 +26,35 @@ export const AuthContext = createContext<AuthContextType>({
   email: "",
   password: "",
   otp: "",
+  verificationType: VerificationType.ConfirmEmail,
+  message: "",
   setEmail: () => {},
   setPassword: () => {},
   setOtp: () => {},
+  setVerificationType: () => {},
+  setMessage: () => {},
 });
 
 export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [otp, setOtp] = useState("");
+  const [verificationType, setVerificationType] = useState<VerificationType>(
+    VerificationType.ConfirmEmail
+  );
+  const [message, setMessage] = useState("");
 
   const authContextValue: AuthContextType = {
     email,
     password,
     otp,
+    verificationType,
+    message,
     setEmail,
     setPassword,
     setOtp,
+    setVerificationType,
+    setMessage,
   };
 
   return (
