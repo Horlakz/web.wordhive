75bfe8353fae49d9f8bd25f70f094693afaf4f21
horlakz <officialhorlakz@gmail.com>
2023-07-20 15:38:28 +0100

    feat(src/components/): creates otp in put component

---
+++ src/components/OtpInput.tsx
@@ -0,0 +1,169 @@
+import React, { useMemo } from "react";
+
+interface OtpProps {
+  value: string;
+  valueLength: number;
+  onChange: (value: string) => void;
+}
+
+const RE_DIGIT = new RegExp(/^\d+$/);
+
+/**
+ * Component for rendering an OTP input field.
+ * @param value - The current value of the OTP input field.
+ * @param valueLength - The length of the OTP input field.
+ * @param onChange - The function to call when the OTP input field value changes.
+ */
+function OtpInput({ value, valueLength, onChange }: OtpProps) {
+  const valueItems = useMemo(() => {
+    const valueArray = value.split("");
+    const items: Array<string> = [];
+
+    for (let i = 0; i < valueLength; i++) {
+      const char = valueArray[i];
+
+      if (new RegExp(/^\d+$/).test(char)) {
+        items.push(char);
+      } else {
+        items.push("");
+      }
+    }
+
+    return items;
+  }, [value, valueLength]);
+
+  const focusToNextInput = (target: HTMLElement) => {
+    const nextElementSibling =
+      target.nextElementSibling as HTMLInputElement | null;
+
+    if (nextElementSibling) {
+      nextElementSibling.focus();
+    }
+  };
+
+  const focusToPrevInput = (target: HTMLElement) => {
+    const previousElementSibling =
+      target.previousElementSibling as HTMLInputElement | null;
+
+    if (previousElementSibling) {
+      previousElementSibling.focus();
+    }
+  };
+
+  const inputOnChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    idx: number
+  ) => {
+    const target = e.target;
+    let targetValue = target.value;
+
+    const isTargetValueDigit = RE_DIGIT.test(targetValue);
+
+    if (!isTargetValueDigit && targetValue !== "") {
+      return;
+    }
+
+    const nextInputEl = target.nextElementSibling as HTMLInputElement | null;
+
+    // only delete digit if next input element has no value
+    if (!isTargetValueDigit && nextInputEl && nextInputEl.value !== "") {
+      return;
+    }
+
+    targetValue = isTargetValueDigit ? targetValue : " ";
+
+    if (!RE_DIGIT.test(targetValue)) {
+      return;
+    }
+
+    const targetValueLength = targetValue.length;
+
+    if (targetValueLength === 1) {
+      const newValue =
+        value.substring(0, idx) + targetValue + value.substring(idx + 1);
+
+      onChange(newValue);
+
+      if (!isTargetValueDigit) {
+        return;
+      }
+
+      const nextElementSibling =
+        target.nextElementSibling as HTMLInputElement | null;
+
+      if (nextElementSibling) {
+        nextElementSibling.focus();
+      }
+    } else if (targetValueLength === valueLength) {
+      onChange(targetValue);
+
+      target.blur();
+    }
+  };
+
+  const inputOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    const { key } = e;
+
+    if (key === "ArrowRight" || key === "ArrowDown") {
+      e.preventDefault();
+      return focusToNextInput(target);
+    }
+
+    if (key === "ArrowLeft" || key === "ArrowUp") {
+      e.preventDefault();
+      return focusToPrevInput(target);
+    }
+
+    const targetValue = target.value;
+
+    // keep the selection range position
+    // if the same digit was typed
+    target.setSelectionRange(0, targetValue.length);
+
+    if (e.key !== "Backspace" || target.value !== "") {
+      return;
+    }
+
+    const previousElementSibling =
+      target.previousElementSibling as HTMLInputElement | null;
+
+    if (previousElementSibling) {
+      previousElementSibling.focus();
+    }
+  };
+
+  const inputOnFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { target } = e;
+
+    const prevInputEl =
+      target.previousElementSibling as HTMLInputElement | null;
+
+    if (prevInputEl && prevInputEl.value === "") {
+      return prevInputEl.focus();
+    }
+
+    target.setSelectionRange(0, target.value.length);
+  };
+  return (
+    <div className="flex gap-4">
+      {valueItems.map((digit, index) => (
+        <input
+          key={index}
+          type="text"
+          inputMode="numeric"
+          autoComplete="one-time-code"
+          pattern="\d{1}"
+          maxLength={valueLength}
+          value={digit}
+          onChange={(e) => inputOnChange(e, index)}
+          onKeyDown={inputOnKeyDown}
+          onFocus={inputOnFocus}
+          className="w-12 h-12 text-center border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary"
+        />
+      ))}
+    </div>
+  );
+}
+
+export default OtpInput;
