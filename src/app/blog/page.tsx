76d92c5a1aee3a0dc2c65e62b6ef4f5219db35a9
horlakz <officialhorlakz@gmail.com>
2023-07-21 12:33:23 +0100

    feat(src/app/blog/): adds link to page  to blog post

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import dot from "@/assets/icons/dot.svg";
 
@@ -31,39 +32,41 @@ const Blog = () => {
           });
 
           return (
-            <div key={i} className="border-2 rounded-2xl space-y-6 p-4">
-              <h6 className="text-xl text-dark-900 mt-2 font-semibold">
-                Blog Title {i + 1}
-              </h6>
+            <Link key={i} href={`/blog/${i}`} className="cursor-pointer">
+              <div className="border-2 rounded-2xl space-y-6 p-4 hover:bg-[#e5e5e5] default-transition">
+                <h6 className="text-xl text-dark-900 mt-2 font-semibold">
+                  Blog Title {i + 1}
+                </h6>
 
-              <p className="text-dark-600 line-clamp-6">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad,
-                amet. Animi dolorum tempora molestiae labore laudantium repellat
-                officia hic odit, veritatis eligendi? Esse distinctio dolorum
-                neque eveniet consequatur veritatis repellendus ab, quos numquam
-                eaque animi laudantium praesentium accusantium ratione optio,
-                quas id, dolore qui deleniti vel dignissimos! Ullam, tenetur at
-                eius unde tempora quasi rerum voluptatum error ad accusantium.
-                Neque natus consectetur hic praesentium temporibus ex eveniet
-                nisi, deserunt ab vitae doloribus, cupiditate ipsum maxime ea
-                dicta explicabo voluptates numquam eligendi necessitatibus
-                beatae quae voluptatibus incidunt. Impedit voluptate, id quae
-                praesentium nulla odio architecto asperiores consequuntur
-                numquam perferendis inventore ea!
-              </p>
+                <p className="text-dark-600 line-clamp-6">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad,
+                  amet. Animi dolorum tempora molestiae labore laudantium
+                  repellat officia hic odit, veritatis eligendi? Esse distinctio
+                  dolorum neque eveniet consequatur veritatis repellendus ab,
+                  quos numquam eaque animi laudantium praesentium accusantium
+                  ratione optio, quas id, dolore qui deleniti vel dignissimos!
+                  Ullam, tenetur at eius unde tempora quasi rerum voluptatum
+                  error ad accusantium. Neque natus consectetur hic praesentium
+                  temporibus ex eveniet nisi, deserunt ab vitae doloribus,
+                  cupiditate ipsum maxime ea dicta explicabo voluptates numquam
+                  eligendi necessitatibus beatae quae voluptatibus incidunt.
+                  Impedit voluptate, id quae praesentium nulla odio architecto
+                  asperiores consequuntur numquam perferendis inventore ea!
+                </p>
 
-              <div className="flex items-center text-sm">
-                <span>Category</span>
-                <Image
-                  src={dot}
-                  alt="dot"
-                  width={4}
-                  height={4}
-                  className="mx-2"
-                />
-                <span>{randomDate}</span>
+                <div className="flex items-center text-sm">
+                  <span>Category</span>
+                  <Image
+                    src={dot}
+                    alt="dot"
+                    width={4}
+                    height={4}
+                    className="mx-2"
+                  />
+                  <span>{randomDate}</span>
+                </div>
               </div>
-            </div>
+            </Link>
           );
         })}
       </section>
