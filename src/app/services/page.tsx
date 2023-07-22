7d51a7769a6248e15c3c47219f1b163d15b89b73
horlakz <officialhorlakz@gmail.com>
2023-07-22 18:34:30 +0100

    feat(src/app/services/): import services data, adds key prop to category iterator and updates Service Card props;

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,26 +1,28 @@
-import dummy from "@/assets/images/dummy.png";
 import ServiceCard from "@/components/services/Card";
+import { services } from "./data";
 
 const ServicesPage = () => {
   return (
     <main className="py-5 sm:px-20 px-8">
       <section className="flex-center flex-wrap gap-5">
         {Array.from({ length: 6 }).map((_, i) => (
-          <span className="text-lg text-dark-600 hover:text-secondary default-transition cursor-pointer">
-            Category
+          <span
+            key={i}
+            className="text-lg text-dark-600 hover:text-secondary default-transition cursor-pointer"
+          >
+            Category {i}
           </span>
         ))}
       </section>
 
       <section className="grid sm:grid-cols-3 gap-10 py-5">
-        {Array.from({ length: 6 }).map((_, i) => (
+        {services.map((service, i) => (
           <ServiceCard
-            image={dummy}
-            title={`Service Title ${i}`}
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-          quas dolor voluptatibus odit architecto debitis obcaecati nobis
-          ducimus accusamus unde?"
-            price={20}
+            key={i}
+            image={service.image}
+            title={`${service.title} ${i}`}
+            body={service.body}
+            volumes={service.volumes}
           />
         ))}
       </section>
