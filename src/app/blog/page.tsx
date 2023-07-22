1c9e415666f5e532b00062337a4ac79b9440c618
horlakz <officialhorlakz@gmail.com>
2023-07-22 18:36:34 +0100

    fix(src/app/blog/):adds key to element iterator

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,7 +9,10 @@ const Blog = () => {
     <main className="py-5 sm:px-20 px-8">
       <section className="flex-center flex-wrap gap-5">
         {Array.from({ length: 6 }).map((_, i) => (
-          <span className="text-lg text-dark-600 hover:text-secondary default-transition cursor-pointer">
+          <span
+            key={i}
+            className="text-lg text-dark-600 hover:text-secondary default-transition cursor-pointer"
+          >
             Category
           </span>
         ))}
