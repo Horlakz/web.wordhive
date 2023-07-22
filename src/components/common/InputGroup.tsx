49ab22783e34a7080f5caa48f70a4ec00f080a9e
horlakz <officialhorlakz@gmail.com>
2023-07-22 18:32:09 +0100

    feat(src/component/common/): adds Select Component with props

--- src/components/common/InputGroup.tsx
+++ src/components/common/InputGroup.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+interface Option {
+  label: string;
+  value: string;
+}
+
 interface InputProps<T = HTMLInputElement> {
   className?: string;
   label: string;
@@ -11,6 +16,12 @@ interface InputProps<T = HTMLInputElement> {
 
 interface TextAreaProps extends Omit<InputProps<HTMLTextAreaElement>, "type"> {
   rows?: number;
+}
+
+interface SelectProps
+  extends Omit<InputProps<HTMLSelectElement>, "type" | "placeholder"> {
+  options: Option[];
+  defaultOption?: Option;
 }
 
 function Input({
@@ -73,9 +84,62 @@ function TextArea({
   );
 }
 
+function Select({
+  className,
+  label = "Select an option",
+  options,
+  defaultOption,
+  value,
+  onChange,
+}: SelectProps) {
+  function ConvertLabelToDefaultOption(text: string) {
+    const textToLowercase = text.toLocaleLowerCase();
+    const selectIsPresent = textToLowercase.includes("select");
+    const replaceText = textToLowercase.replace("select", "choose a");
+
+    if (selectIsPresent) {
+      return replaceText;
+    }
+    return `Choose a ${textToLowercase}`;
+  }
+
+  return (
+    <div className={className}>
+      <label
+        htmlFor="countries"
+        className="block mb-2 text-sm font-medium text-gray-900"
+      >
+        {label}
+      </label>
+      <select
+        id="countries"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 "
+        value={value}
+        onChange={onChange}
+      >
+        <option
+          defaultValue={defaultOption?.value ? defaultOption.value : ""}
+          value={defaultOption?.value ? defaultOption.value : ""}
+          className="capitalize"
+        >
+          {defaultOption?.label
+            ? defaultOption.label
+            : ConvertLabelToDefaultOption(label)}
+        </option>
+        {options.map((option, i) => (
+          <option key={i} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
 const InputGroup = {
   Input,
   TextArea,
+  Select,
 };
 
 export default InputGroup;
