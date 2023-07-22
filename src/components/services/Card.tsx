cd708c871c35c0fbf30f9b2ec44e1bb7e1f7d4d0
horlakz <officialhorlakz@gmail.com>
2023-07-22 18:29:30 +0100

    feat(src/components/services/): creates Service Card component and Details component for modal

--- src/components/services/Card.tsx
+++ src/components/services/Card.tsx
@@ -1,36 +1,70 @@
-import React from "react";
+"use client";
+
 import Image, { type StaticImageData } from "next/image";
+import React from "react";
 
-import Button from "../common/Button";
+import Button from "@/components/common/Button";
+import Modal from "@/components/common/Modal";
+import Details from "./Details";
 
-interface CardProps {
-  image: string | StaticImageData;
-  title: string;
-  description: string;
+interface Quality {
+  type: string;
   price: number;
 }
 
-function ServiceCard({ image, title, description, price }: CardProps) {
+interface Volume {
+  name: string;
+  qualities: Quality[];
+}
+
+export interface CardProps {
+  image: string | StaticImageData;
+  title: string;
+  body: string;
+  volumes: Volume[];
+}
+
+function ServiceCard({ image, title, body, volumes }: CardProps) {
+  const [showModal, setShowModal] = React.useState(false);
+  const [price, setPrice] = React.useState(volumes[0].qualities[0].price);
+
   return (
-    <div className="border-2 rounded-2xl p-4">
-      <div className="relative overflow-hidden rounded-3xl">
-        <Image src={image} height={250} alt={title} className="rounded-3xl" />
+    <>
+      <div className="border-2 rounded-2xl p-4">
+        <div className="relative overflow-hidden rounded-3xl">
+          <Image src={image} height={250} alt={title} className="rounded-3xl" />
 
-        <span className="absolute -top-5 -right-4 bg-secondary text-white text-xl pr-10 pl-6 pt-6 pb-2 font-semibold rounded-3xl">
-          ${price}
-        </span>
+          <span className="absolute -top-5 -right-4 bg-secondary text-white text-xl pr-10 pl-6 pt-6 pb-2 font-semibold rounded-3xl">
+            ${price}
+          </span>
+        </div>
+
+        <h4 className="text-2xl text-dark-900 mt-2 font-semibold">{title}</h4>
+
+        <p className="text-dark-600 line-clamp-4">{body}</p>
+
+        <div className="flex w-full justify-end">
+          <Button
+            colorScheme="secondary"
+            onClick={() => setShowModal(true)}
+            className="mt-4"
+          >
+            Order Service
+          </Button>
+        </div>
       </div>
 
-      <h6 className="text-2xl text-dark-900 mt-2 font-semibold">{title}</h6>
-
-      <p className="text-dark-600 line-clamp-4">{description}</p>
-
-      <div className="flex w-full justify-end">
-        <Button variant="outline" className="p-0 border-none">
-          View Full Details
-        </Button>
-      </div>
-    </div>
+      <Modal visibility={showModal} setVisibility={() => setShowModal(false)}>
+        <Details
+          price={price}
+          setPrice={setPrice}
+          image={image}
+          title={title}
+          body={body}
+          volumes={volumes}
+        />
+      </Modal>
+    </>
   );
 }
 
---
+++ src/components/services/Details.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Image from "next/image";
+
+import InputGroup from "@/components/common/InputGroup";
+import Button from "@/components/common/Button";
+import { CardProps } from "./Card";
+
+interface Props extends CardProps {
+  price: number;
+  setPrice: (price: number) => void;
+}
+
+function Details({ title, image, body, volumes, price, setPrice }: Props) {
+  const [selectedQuality, setSelectedQuality] = React.useState<string>(
+    volumes[0].name
+  );
+
+  const qualities =
+    volumes.find((vol) => vol.name === selectedQuality)?.qualities || [];
+
+  return (
+    <div aria-label="modal" className="sm:p-10 p-4 sm:w-[40rem] space-y-4">
+      <div className="flex sm:flex-row flex-col sm:items-center gap-6">
+        <Image
+          src={image}
+          height={500}
+          width={500}
+          alt={title}
+          className="sm:w-1/4"
+        />
+
+        <div className="space-y-2">
+          <h4 className="text-2xl font-semibold">{title}</h4>
+          <p className="text-dark-600">{body}</p>
+        </div>
+      </div>
+
+      <InputGroup.Select
+        label="Select Service Quality"
+        onChange={(e) => setSelectedQuality(e.target.value)}
+        options={volumes.slice(1).map((vol) => {
+          return {
+            label: vol.name,
+            value: vol.name,
+          };
+        })}
+        defaultOption={{ label: volumes[0].name, value: volumes[0].name }}
+      />
+      <InputGroup.Select
+        label="Select Volume(Pages)"
+        onChange={(e) => setPrice(Number(e.target.value))}
+        options={
+          qualities?.slice(1).map((qual) => {
+            return {
+              label: qual.type,
+              value: qual.price.toLocaleString(),
+            };
+          }) || []
+        }
+        defaultOption={{
+          label: qualities[0].type,
+          value: qualities[0].price.toLocaleString(),
+        }}
+      />
+
+      <div className="flex items-center gap-2 w-full">
+        <div className="flex justify-between items-center bg-[#f5f5f5] text-lg font-semibold px-3 py-2 w-full rounded-lg">
+          <span>Price</span>
+          <span className="text-secondary">${price}</span>
+        </div>
+
+        <Button colorScheme="secondary" className="w-full flex-center">
+          Pay Now
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Details;
