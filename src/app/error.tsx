29e076b3b714fbffd15baeb686b94eddbb06c0f6
horlakz <officialhorlakz@gmail.com>
2023-07-27 06:04:25 +0100

    feat(src/app/): adds new state app without design

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <p>An Error Occured While Trying to Get this Page</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <p>page not NotFoundPage</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
