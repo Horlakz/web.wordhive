bf90b49bc4febbcf54a1ad68ba06edcb0bd9b074
horlakz <officialhorlakz@gmail.com>
2023-07-27 07:24:45 +0100

    feat(src/app/portfolio/): integrates api to page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,41 +1,42 @@
 import Image from "next/image";
-import React from "react";
+import { Key } from "react";
 
-import dummy from "@/assets/images/dummy.png";
 import success from "@/assets/icons/success.svg";
+import { PortfolioData, PortfolioService } from "@/services/portfolio";
 
-const Portfolio = () => {
+const portfolioService = new PortfolioService();
+
+const Portfolio = async () => {
+  const { data } = await portfolioService.listPortfolios();
+
   return (
-    <main className="grid sm:grid-cols-3 sm:p-20 p-6 gap-10">
-      {[...new Array(10)].map((portfolio, i) => (
+    <main className="grid sm:grid-cols-3 sm:p-10 p-6 gap-10">
+      {data.results.map((portfolio: PortfolioData, i: Key) => (
         <div key={i} className="border-2 rounded-2xl p-4">
           <Image
-            src={dummy}
+            src={"http://localhost:8000/v1/media/" + portfolio.image}
+            width={250}
             height={250}
-            alt={i.toString()}
+            alt={portfolio.title}
             className="rounded-3xl"
           />
 
           <h6 className="text-2xl text-dark-900 mt-2 font-semibold">
-            Portfolio Title {i + 1}
+            {portfolio.title}
           </h6>
 
-          <span className="text-[#A3A3A3] text-sm">Category</span>
+          <span className="text-[#A3A3A3] text-sm">{portfolio.field.name}</span>
 
-          <p className="text-dark-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quas
-            dolor voluptatibus odit architecto debitis obcaecati nobis ducimus
-            accusamus unde?
-          </p>
+          <p className="text-dark-600">{portfolio.body}</p>
 
           <div className="flex items-center gap-x-6 gap-y-2 flex-wrap mt-6">
-            {[...new Array(Math.floor(Math.random() * 4) + 1)].map((tag, i) => (
+            {portfolio.genre.map((genre, i) => (
               <div
                 key={i}
                 className="flex-center bg-secondary bg-opacity-10 w-fit gap-1 p-1 rounded-full"
               >
                 <Image src={success} alt="success icon" width={20} />
-                <span className="text-xs text-secondary">First Tag</span>
+                <span className="text-xs text-secondary">{genre.name}</span>
               </div>
             ))}
           </div>
