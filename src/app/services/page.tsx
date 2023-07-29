8bfc53eff695c8ac7af8fc3cda92d8a6240fbc23
horlakz <officialhorlakz@gmail.com>
2023-07-29 11:20:05 +0100

    feat(src/app/services/): integrates service api into page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,26 +1,58 @@
-import ServiceCard from "@/components/services/Card";
-import { services } from "./data";
+import { StaticImageData } from "next/image";
 
-const ServicesPage = () => {
+import dummy from "@/assets/images/dummy.png";
+import ServiceCard from "@/components/services/Card";
+import { ApplicationService } from "@/services/services";
+import { ApplicationServiceCategory } from "@/services/services/category";
+
+const applicationService = new ApplicationService();
+const applicationServiceCategory = new ApplicationServiceCategory();
+
+interface ServicesData {
+  uuid: string;
+  title: string;
+  image: StaticImageData | string;
+  body: string;
+  category: { name: string };
+  volumes: {
+    name: string;
+    qualities: {
+      type: string;
+      price: number;
+    }[];
+  }[];
+}
+[];
+
+const ServicesPage = async () => {
+  const servicesData = await applicationService.listServices();
+  const categoriesData =
+    await applicationServiceCategory.listServiceCategories();
+
+  const [services, categories] = await Promise.all([
+    servicesData,
+    categoriesData,
+  ]);
+
   return (
     <main className="py-5 sm:px-20 px-8">
       <section className="flex-center flex-wrap gap-5">
-        {Array.from({ length: 6 }).map((_, i) => (
+        {categories.data.map((category: { name: string; uuid: string }) => (
           <span
-            key={i}
+            key={category.uuid}
             className="text-lg text-dark-600 hover:text-secondary default-transition cursor-pointer"
           >
-            Category {i}
+            {category.name}
           </span>
         ))}
       </section>
 
       <section className="grid sm:grid-cols-3 gap-10 py-5">
-        {services.map((service, i) => (
+        {services.data.map((service: ServicesData) => (
           <ServiceCard
-            key={i}
-            image={service.image}
-            title={`${service.title} ${i}`}
+            key={service.uuid}
+            image={dummy}
+            title={service.title}
             body={service.body}
             volumes={service.volumes}
           />
