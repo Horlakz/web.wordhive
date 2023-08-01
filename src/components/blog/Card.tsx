3baf733e347de043e57f56f17c49cd2206fa9a02
horlakz <officialhorlakz@gmail.com>
2023-08-01 19:51:13 +0100

    feat(src/components/blog/): adds dynamic body to blog card

--- src/components/blog/Card.tsx
+++ src/components/blog/Card.tsx
@@ -18,20 +18,7 @@ function BlogCard({ slug, date, title, body, category }: BlogProps) {
       <div className="border-2 rounded-2xl space-y-6 p-4 hover:bg-[#e5e5e5] default-transition">
         <h6 className="text-xl text-dark-900 mt-2 font-semibold">{title}</h6>
 
-        <p className="text-dark-600 line-clamp-6">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, amet.
-          Animi dolorum tempora molestiae labore laudantium repellat officia hic
-          odit, veritatis eligendi? Esse distinctio dolorum neque eveniet
-          consequatur veritatis repellendus ab, quos numquam eaque animi
-          laudantium praesentium accusantium ratione optio, quas id, dolore qui
-          deleniti vel dignissimos! Ullam, tenetur at eius unde tempora quasi
-          rerum voluptatum error ad accusantium. Neque natus consectetur hic
-          praesentium temporibus ex eveniet nisi, deserunt ab vitae doloribus,
-          cupiditate ipsum maxime ea dicta explicabo voluptates numquam eligendi
-          necessitatibus beatae quae voluptatibus incidunt. Impedit voluptate,
-          id quae praesentium nulla odio architecto asperiores consequuntur
-          numquam perferendis inventore ea!
-        </p>
+        <p className="text-dark-600 line-clamp-6">{body}</p>
 
         <div className="flex items-center text-sm">
           <span>{category}</span>
