9c80c993c4d4e45df91c29f8346f72c51a5393c7
horlakz <officialhorlakz@gmail.com>
2023-08-06 23:46:44 +0100

    feat(/components/baselayout/): creates nav menu and adds animation

--- src/components/baselayout/Header.tsx
+++ src/components/baselayout/Header.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 import menu from "@/assets/icons/menu.svg";
 import Button from "@/components/common/Button";
@@ -9,6 +10,29 @@ import { AuthContext } from "@/store/context/auth";
 import navLinks from "@/store/data/nav-links.json";
 import AuthModal from "../auth";
 import Storage from "@/utilities/storage";
+import classNames from "classnames";
+
+const AuthActionBtn = ({ isAuth }: { isAuth: boolean }) => {
+  const router = useRouter();
+  const { setModal } = useContext(AuthContext);
+
+  return (
+    <Button
+      variant="outline"
+      colorScheme="danger"
+      className="border-none"
+      onClick={() => {
+        if (isAuth) {
+          router.push("/account");
+        } else {
+          setModal(true);
+        }
+      }}
+    >
+      {isAuth ? "Account" : "Login"}
+    </Button>
+  );
+};
 
 const Header = () => {
   const router = useRouter();
@@ -16,6 +40,30 @@ const Header = () => {
   const storage = new Storage();
   const isAuth = storage.checkCookie("access");
   const { setModal } = useContext(AuthContext);
+  const [nav, showNav] = useState(false);
+
+  const navVariants = {
+    hidden: {
+      opacity: 0,
+      y: -50,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.2,
+        ease: "easeInOut",
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: -50,
+      transition: {
+        duration: 0.2,
+        ease: "easeInOut",
+      },
+    },
+  };
 
   return (
     <header className="flex justify-between items-center capitalize sm:px-16 px-8 py-4 shadow-lg">
@@ -66,7 +114,70 @@ const Header = () => {
         </Button>
       </div>
 
-      <Image src={menu} alt="menu" className="sm:hidden" />
+      <div className="relative sm:hidden">
+        <button onClick={() => showNav(!nav)}>
+          <span
+            className={classNames(
+              "menu-icon cursor-pointer",
+              nav ? "active" : ""
+            )}
+          />
+        </button>
+
+        <AnimatePresence>
+          {nav && (
+            <motion.ul
+              variants={navVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              className="absolute top-14 right-0 flex flex-col items-end bg-white gap-4 px-4 w-40"
+            >
+              {[
+                ...navLinks,
+                { name: isAuth ? "Account" : "", path: "/account" },
+              ].map((link, i) => {
+                const isActive = pathname.startsWith(link.path);
+
+                if (i === navLinks.length) {
+                  return (
+                    <motion.li>
+                      <Button
+                        variant="outline"
+                        colorScheme="danger"
+                        className="border-none m-0 p-0"
+                        onClick={() => {
+                          if (isAuth) {
+                            storage.removeCookie("access");
+                            router.push("/");
+                          } else {
+                            setModal(true);
+                          }
+
+                          showNav(false);
+                        }}
+                      >
+                        {isAuth ? "Logout" : "Login"}
+                      </Button>
+                    </motion.li>
+                  );
+                }
+
+                return (
+                  <motion.li key={i} onClick={() => showNav(false)}>
+                    <Link
+                      className={isActive ? "text-primary" : "text-black"}
+                      href={link.path}
+                    >
+                      {link.name}
+                    </Link>
+                  </motion.li>
+                );
+              })}
+            </motion.ul>
+          )}
+        </AnimatePresence>
+      </div>
 
       <AuthModal />
     </header>
