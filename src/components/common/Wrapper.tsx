abb12e33c5fe92ffa1f7bee020363d00bc5e6aa5
horlakz <officialhorlakz@gmail.com>
2023-08-10 19:40:51 +0100

    feat(component/common/): creates all layout wrapper

---
+++ src/components/common/Wrapper.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
+
+function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default LayoutWrapper;
