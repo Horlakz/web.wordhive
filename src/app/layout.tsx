2a52432ba640c88d027f06847511909a40b32079
horlakz <officialhorlakz@gmail.com>
2023-08-10 19:50:33 +0100

    feat(app/layout/): creates base layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { Metadata } from "next";
+
+import Wrapper from "@/components/common/Wrapper";
+import "@/styles/globals.scss";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Wordhive",
+    default: "Wordhive",
+  },
+  icons: "/images/logo.png",
+  description: "For Write Code, Write Docs, Write Anything",
+  keywords: "wordhive, word, hive, write, code, docs, anything",
+  authors: [{ name: "Horlakz", url: "https://github.com/horlakz" }],
+  creator: "Horlakz",
+  metadataBase: new URL("https://wordhive.com"),
+  openGraph: {
+    title: "Wordhive",
+    description: "For Write Code, Write Docs, Write Anything",
+    siteName: "Wordhive",
+    type: "website",
+    locale: "en_US",
+    images: [{ url: "/logo.png" }],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Wrapper>{children}</Wrapper>
+      </body>
+    </html>
+  );
+}
