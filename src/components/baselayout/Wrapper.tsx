6dee0aa406bfdd194e9b52f7634f0679dd39db06
horlakz <officialhorlakz@gmail.com>
2023-08-10 19:43:29 +0100

    enhancement(components/baselayout/): extracts base components to layout wrapper

--- src/components/baselayout/Wrapper.tsx
+++ src/components/baselayout/Wrapper.tsx
@@ -1,30 +1,21 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
-import { Toaster } from "react-hot-toast";
 
 import { AuthProvider } from "@/store/context/auth";
 import Footer from "./Footer";
 import Header from "./Header";
 
-const queryClient = new QueryClient();
-
-function Wrapper({ children }: { children: React.ReactNode }) {
+function ClientLayoutWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster />
-      <div>
-        <AuthProvider>
-          <Header />
-        </AuthProvider>
-        {children}
-        <Footer />
-      </div>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <div>
+      <AuthProvider>
+        <Header />
+      </AuthProvider>
+      {children}
+      <Footer />
+    </div>
   );
 }
 
-export default Wrapper;
+export default ClientLayoutWrapper;
