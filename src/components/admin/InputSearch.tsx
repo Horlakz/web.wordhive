2cc4cf47e5039179c13c22b83b03540e8160652c
horlakz <officialhorlakz@gmail.com>
2023-08-14 07:16:26 +0100

    feat(/components/admin/): creates component for input search

---
+++ src/components/admin/InputSearch.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+interface InputSearchProps {
+  placeholder?: string;
+}
+
+function InputSearch({ placeholder }: InputSearchProps) {
+  return (
+    <div>
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 "
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-[35rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none focus:border-admin-light default-transition"
+          placeholder={placeholder || "Enter your search term here"}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default InputSearch;
