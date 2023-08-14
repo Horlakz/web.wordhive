081decd2b268607d913c8b7f54f222918036f566
horlakz <officialhorlakz@gmail.com>
2023-08-14 06:52:40 +0100

    feat(/components/common/): creates dynamic table component

---
+++ src/components/common/Table.tsx
@@ -0,0 +1,67 @@
+interface TableProps {
+  tableHeaders: {
+    title: string;
+    className?: string;
+    colSpan?: number;
+  }[];
+  tableActions?: ((data: any) => JSX.Element)[];
+  tableKeys: string[];
+  tableData: any[];
+}
+
+function Table({
+  tableHeaders,
+  tableActions,
+  tableKeys,
+  tableData,
+}: TableProps) {
+  return (
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="bg-admin-primary text-white capitalize">
+          <tr>
+            {tableHeaders.map((header, index) => (
+              <th
+                key={index}
+                scope="col"
+                className={`px-4 py-2 font-medium ${header?.className || ""}`}
+                colSpan={header?.colSpan || 1}
+              >
+                {header.title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {tableData?.map((data, index) => (
+            <tr className="border-b" key={index}>
+              {tableKeys.map((key, index) => (
+                <th
+                  scope="row"
+                  className={`px-6 py-4 font-medium text-dark-900 whitespace-nowrap`}
+                  key={index}
+                >
+                  {data[key]}
+                </th>
+              ))}
+
+              {/* table actions */}
+              <th
+                scope="row"
+                className="flex-center px-6 py-4 font-medium text-gray-800 space-x-2 whitespace-nowrap"
+              >
+                {tableActions &&
+                  tableActions.map((action, index) => (
+                    <span key={index}>{action(data)}</span>
+                  ))}
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
