2dcc7731434a21834f53809333e05c1281450b82
horlakz <officialhorlakz@gmail.com>
2023-08-15 16:03:30 +0100

    enhancement(/components/common/): updates table styles

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -19,12 +19,14 @@ function Table({
     <div className="relative overflow-x-auto">
       <table className="w-full text-sm text-left text-gray-500">
         <thead className="bg-admin-primary text-white capitalize">
-          <tr>
+          <tr className="w-full flex justify-between items-center">
             {tableHeaders.map((header, index) => (
               <th
                 key={index}
                 scope="col"
-                className={`px-4 py-2 font-medium ${header?.className || ""}`}
+                className={`w-full px-4 py-2 font-medium ${
+                  header?.className || ""
+                }`}
                 colSpan={header?.colSpan || 1}
               >
                 {header.title}
@@ -34,28 +36,31 @@ function Table({
         </thead>
 
         <tbody>
-          {tableData?.map((data, index) => (
-            <tr className="border-b" key={index}>
+          {tableData.map((data, index) => (
+            <tr
+              className="w-full border-b flex justify-between items-center"
+              key={index}
+            >
               {tableKeys.map((key, index) => (
-                <th
+                <td
                   scope="row"
-                  className={`px-6 py-4 font-medium text-dark-900 whitespace-nowrap`}
+                  className={`w-full px-6 py-4 font-medium text-dark-900 line-clamp-1 whitespace-nowrap`}
                   key={index}
                 >
                   {data[key]}
-                </th>
+                </td>
               ))}
 
               {/* table actions */}
-              <th
+              <td
                 scope="row"
-                className="flex items-center px-6 py-4 font-medium space-x-2 whitespace-nowrap"
+                className="w-full flex items-center px-6 py-4 font-medium space-x-2 whitespace-nowrap"
               >
                 {tableActions &&
                   tableActions.map((action, index) => (
                     <span key={index}>{action(data)}</span>
                   ))}
-              </th>
+              </td>
             </tr>
           ))}
         </tbody>
