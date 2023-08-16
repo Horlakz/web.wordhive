611076f346bc435b5b2256725f8136856e077275
horlakz <officialhorlakz@gmail.com>
2023-08-16 14:51:44 +0100

    fix(/app/(client/):portfolio/): updates api integration in page

--- src/app/(client)/portfolio/page.tsx
+++ src/app/(client)/portfolio/page.tsx
@@ -3,6 +3,8 @@ import { Key } from "react";
 
 import success from "@/assets/icons/success.svg";
 import { PortfolioData, PortfolioService } from "@/services/portfolio";
+import { PortfolioFieldData } from "@/services/portfolio/field";
+import { PortfolioGenreData } from "@/services/portfolio/genre";
 
 const portfolioService = new PortfolioService();
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
@@ -13,25 +15,27 @@ const Portfolio = async () => {
   return (
     <main className="grid sm:grid-cols-3 sm:p-10 p-6 gap-10">
       {data.results.map((portfolio: PortfolioData, i: Key) => (
-        <div key={i} className="border-2 rounded-2xl p-4">
+        <div key={i} className="border-2 rounded-2xl p-4 w-80 h-fit">
           <Image
             src={apiUrl + "/media/" + portfolio.image}
             width={250}
             height={250}
             alt={portfolio.title}
-            className="rounded-3xl"
+            className="rounded-3xl w-72 h-64"
           />
 
           <h6 className="text-2xl text-dark-900 mt-2 font-semibold">
             {portfolio.title}
           </h6>
 
-          <span className="text-[#A3A3A3] text-sm">{portfolio.field.name}</span>
+          <span className="text-[#A3A3A3] text-sm">
+            {(portfolio.field as PortfolioFieldData)["name"]}
+          </span>
 
-          <p className="text-dark-600">{portfolio.body}</p>
+          <p className="text-dark-600 line-clamp-4">{portfolio.body}</p>
 
           <div className="flex items-center gap-x-6 gap-y-2 flex-wrap mt-6">
-            {portfolio.genre.map((genre, i) => (
+            {(portfolio.genres as PortfolioGenreData[]).map((genre, i) => (
               <div
                 key={i}
                 className="flex-center bg-secondary bg-opacity-10 w-fit gap-1 p-1 rounded-full"
