adc61d973de096274e799af4bf4e7727712ef7c4
horlakz <officialhorlakz@gmail.com>
2023-08-29 21:04:12 +0100

    feat(src/components/common/): adds default error function to queries

--- src/components/common/Wrapper.tsx
+++ src/components/common/Wrapper.tsx
@@ -2,12 +2,28 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { Toaster } from "react-hot-toast";
 
-const queryClient = new QueryClient();
+import Storage from "@/utilities/storage";
 
 function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  const storage = new Storage();
+  const router = useRouter();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        onError: (error: any) => {
+          if (error.response.status == 401 || error.response.status == 403) {
+            router.push("/");
+            storage.removeCookie("access");
+          }
+        },
+      },
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
