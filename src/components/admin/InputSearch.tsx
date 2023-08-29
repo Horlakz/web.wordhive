006d25d33b2ced98b39e5672f1080780119b1ec8
horlakz <officialhorlakz@gmail.com>
2023-08-29 19:49:08 +0100

    feat(src/components/admin/): adds value and onchange props to component

--- src/components/admin/InputSearch.tsx
+++ src/components/admin/InputSearch.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 interface InputSearchProps {
   placeholder?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function InputSearch({ placeholder }: InputSearchProps) {
+function InputSearch({ placeholder, value, onChange }: InputSearchProps) {
   return (
     <div>
       <label
@@ -34,6 +36,8 @@ function InputSearch({ placeholder }: InputSearchProps) {
         <input
           type="search"
           id="default-search"
+          value={value}
+          onChange={onChange}
           className="block w-[35rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none focus:border-admin-light default-transition"
           placeholder={placeholder || "Enter your search term here"}
         />
