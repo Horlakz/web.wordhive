f7d167f8e16adcc98cce18b30b7e6b8c789a548c
horlakz <officialhorlakz@gmail.com>
2023-08-30 10:28:03 +0100

    enhancement(src/components/common/): removes var from useEffect function

--- src/components/common/ProtectedRoute.tsx
+++ src/components/common/ProtectedRoute.tsx
@@ -9,9 +9,9 @@ import Storage from "@/utilities/storage";
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const storage = new Storage();
   const router = useRouter();
+  const accessCookie = storage.getCookie("access");
 
   useEffect(() => {
-    const accessCookie = storage.getCookie("access");
     if (!accessCookie) {
       router.push("/");
       toast.error("You need to login to access this page", {
